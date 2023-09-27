f18164212767a9f8cf09cc2ab871944bb37520c5
student <student@gnu.ac.in>
2023-09-27 08:53:47 +0530

    27-09

--- app/src/main/java/com/example/mad_practical_8_222172012040/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_8_222172012040/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.example.mad_practical_8_222172012040
 
+import android.app.AlarmManager
+import android.app.Notification.Action
+import android.app.PendingIntent
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +11,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun setalarm (militime:Long,action:String){
+        val intentalarm = Intent(applicationContext,AlarmBroadcastReceiver::class.java)
+        intentalarm.putExtra(AlarmBroadcastReceiver.ALARMKEY,action)
+        val pendingintent = PendingIntent.getBroadcast(applicationContext,4345,intentalarm,PendingIntent.FLAG_UPDATE_CURRENT)
+        val manager=getSystemService(ALARM_SERVICE) as AlarmManager
+        if (action== AlarmBroadcastReceiver.ALARMSTART)
+        {
+            manager.setExact(AlarmManager.RTC_WAKEUP,militime,pendingintent)
+        }
+        else
+        {
+            manager.cancel(pendingintent)
+            sendBroadcast(intentalarm)
+        }
     }
 }
