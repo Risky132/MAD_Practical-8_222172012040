c7b8998fd8732c4537e909bd2a2518160fda5aab
LENOVO <shivampatel19@gnu.ac.in>
2023-10-31 16:44:02 +0530

    8th

--- app/src/main/java/com/example/mad_practical_8_222172012040/AlarmBreoadcastReceiver.kt
+++ app/src/main/java/com/example/mad_practical_8_222172012040/AlarmBreoadcastReceiver.kt
@@ -4,25 +4,29 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 
-class AlarmBreoadcastReceiver : BroadcastReceiver() {
+class AlarmBroadcastReceiver : BroadcastReceiver() {
 
-    companion object
-    {
-        val ALARMKEY="key"
-        val ALARMSTART="start"
-        val ALARMSTOP="stop"
+    companion object {
+        val ALARMKEY = "KEY"
+        val ALARMSTART = "START"
+        val ALARMSTOP = "STOP"
     }
+
     override fun onReceive(context: Context, intent: Intent) {
-        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
-        val data = intent.getStringExtra(ALARMKEY)
-        val intentservice = Intent(context,AlarmService::class.java)
-        if(data== ALARMSTART)
-        {
-            context.startService(intentservice)
-        }
-        else
-        {
-            context.stopService(intentservice)
+        if (intent != null) {
+            val data = intent.getStringExtra(ALARMKEY)
+
+            if (data != null) {
+
+                val intentservice = Intent(context, AlarmService::class.java)
+
+                if (data == ALARMSTART) {
+                    context.startService(intentservice)
+                }
+                else{
+                    context.stopService(intentservice)
+                }
+            }
         }
     }
 }
---
+++ app/src/main/java/com/example/mad_practical_8_222172012040/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.mad_practical_8_222172012040;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+}
